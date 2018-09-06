10cf791cd9083574429d6735c36bcf77261e1ce6
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-09-06 17:44:22 -0400

    FInal but no caps

--- 1.py
+++ 1.py
@@ -2,14 +2,15 @@ print("Hi! what's your name")
 username = input('your name_')
 print ("Hello__" + username)
 answer1 = input ('Spell Hop_')
-if answer1 == "hop" or "Hop":
+if answer1 == "hop":
 	print ("yes")
 	answer2 = input ('Spell Top_')
-	if answer2 == "top" or "Top":
+	if answer2 == "top":
 		print ("yes")
 		answer3 = input ('spell Mop_')
-		if answer3 == "mop" or "Mop":
+		if answer3 == "mop":
 			print ("yes")
 			answer4 = input ('what do you do at a green light?')
-			if answer4 == "GO" or "go":
+			if answer4 == "go":
 				print ("Congrats!")
+			else: print("No silly you GOOOO!")
