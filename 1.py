ec6ad6771646ad88cd581cdd87162e46ae51293d
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-09-06 17:39:16 -0400

    1

---
+++ 1.py
@@ -0,0 +1,15 @@
+print("Hi! what's your name")
+username = input('your name_')
+print ("Hello__" + username)
+answer1 = input ('Spell Hop_')
+if answer1 == "hop" or "Hop":
+	print ("yes")
+	answer2 = input ('Spell Top_')
+	if answer2 == "top" or "Top":
+		print ("yes")
+		answer3 = input ('spell Mop_')
+		if answer3 == "mop" or "Mop":
+			print ("yes")
+			answer4 = input ('what do you do at a green light?')
+			if answer4 == "GO" or "go":
+				print ("Congrats!")
