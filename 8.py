dc6e47ddefcd2cf4842596673ba02e70aca5bdf9
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-09-12 19:10:19 -0400

    written days

--- 8.py
+++ 8.py
@@ -7,4 +7,24 @@ i = h + h/4 - h/100 + h/400
 j = m + 12 * ((14 - m)//12) - 2
 k = (d + i + (31*j)/12)%7
 l = round(k)
-print(l)
+if l == 0:
+	print("Sunday")
+else:
+		if l == 1:
+			print("Monday")
+		else:
+			if l == 2:
+				print("Tuesday")
+			else:
+				if l == 3:
+					print("Wednesday")
+				else:
+					if l == 4:
+						print("Thursday")
+					else:
+						if l == 5:
+							print("Friday")
+						else:
+							if l == 6:
+								print("Saturday")
+
