e82b31aa20e3019dfa5c02323ff82bdacf9ff518
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-09-18 11:27:13 -0400

    grade

--- 10.py
+++ 10.py
@@ -1,26 +1,29 @@
 import sys
-float(sys.argv[1])
 if 4.85<=float(sys.argv[1])<=5:
 	print("You have an A+")
-elif 4.7<=float(sys.argv[1])<4.85:
+elif 4.7<=float(sys.argv[1]):
 	print("You have an A")
-elif 4.5<=float(sys.argv[1])<4.7:
+elif 4.5<=float(sys.argv[1]):
 	print("You have an A-")
-elif 4.2<=float(sys.argv[1])<4.5:
+elif 4.2<=float(sys.argv[1]):
 	print("You have a B+")
-elif 3.85<=float(sys.argv[1])<4.2:
+elif 3.85<=float(sys.argv[1]):
 	print("You have a B")
-elif 3.5<=float(sys.argv[1])<3.85:
+elif 3.5<=float(sys.argv[1]):
 	print("You have a B-")
-elif 3.2<=float(sys.argv[1])<3.5:
+elif 3.2<=float(sys.argv[1]):
 	print("You have a C+")
-elif 2.85<=float(sys.argv[1])<3.2:
+elif 2.85<=float(sys.argv[1]):
 	print("You have a C")
-elif 2.5<=float(sys.argv[1])<2.85:
+elif 2.5<=float(sys.argv[1]):
 	print("You have a C-")
-elif 2<=float(sys.argv[1])<2.5:
+elif 2<=float(sys.argv[1]):
 	print("You have a D+")
-elif 1.5<=float(sys.argv[1])<2:
+elif 1.5<=float(sys.argv[1]):
 	print("You have a D")
+elif 1<=float(sys.argv[1]):
+	print("Your have a D-")
+elif 0<= float(sys.argv[1]):
+	print("You have an F")
 else:
 	print("Submit a number 0-5")
