249b24b5682cbfc48c230cdcd853094c6b86af20
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-09-20 12:16:47 -0400

    while demo

--- 17.py
+++ 17.py
@@ -1,1 +1,10 @@
-while true
+
+while True:
+	try:
+		num = int(input("Pick a number between a and 5: "))
+		if num<1 or num>5:
+			num = int(input("thats not right. Isaid pick a number between 1 and 5: "))
+		else:
+			print("Success")
+	except ValueError:
+		print("Yeah, no thats not an integer.")
