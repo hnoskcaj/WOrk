9022cb751ed3147522d1e36a3c933ed7584f98c5
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-09-27 23:02:24 -0400

    hw p3

--- 26.py
+++ 26.py
@@ -1,7 +1,18 @@
 import random as r
 lits = []
 first = []
-for i in range (6):
-	lits.append(r.randrange(1,271,13))
+count = 0
+for i in range (100):
+	lits.append(r.randrange(100,1000,1))
 print(lits)
-print(int(str(lits[0])[:1]))
+while count < 100:
+	first.append(int(str(lits[count])[:1]))
+	count = count + 1
+#first.append(int(str(lits[1])[:1]))
+#first.append(int(str(lits[2])[:1]))
+#first.append(int(str(lits[3])[:1]))
+#first.append(int(str(lits[4])[:1]))
+#first.append(int(str(lits[5])[:1]))
+
+print(first)
+
