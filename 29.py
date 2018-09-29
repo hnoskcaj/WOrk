657de8b92f23a97bb2f22daa2ec01f00cf87fb63
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-09-29 19:50:14 -0400

    1.2.1

--- 29.py
+++ 29.py
@@ -1,7 +1,7 @@
 import sys
 import random as r
 board = []
-b2 = []
+eb = []
 count = 0
 x = 0
 y = 0
@@ -17,16 +17,24 @@ while count < int(sys.argv[3]):
 
 print(board)
 
-
-#for x, y in zip(range(int(sys.argv[1]))), zip(range(int(sys.argv[2]))):
-while x<= int(sys.argv[1]) and y <= int(sys.argv[2]):
-	if board[1][3] ==1:
-		b2[x][y] = '*'
-	#x = x + 1
-	#y = y + 1
-
 for x in range(len(board)):
 	print(*board[x])
+#for x, y in zip(range(int(sys.argv[1]))), zip(range(int(sys.argv[2]))):
+#while x<= int(sys.argv[1]): #and y <= int(sys.argv[2]):
+#	if board[x][y] ==1:
+#		eb[x][y] = '*'
+	#x = x + 1
+	#y = y + 1
+for x in range(board):
+	if board[x+1][y] == str(1):
+		bomb = bomb + 1
+	if board[x-1][y] == 1:
+		bomb = bomb + 1
+	if board[x][y+1] == 1:
+		bomb = bomb + 1
+	if board[x][y-1] == 1:
+		bomb = bomb + 1
+
 
 #for x, y in zip(range(int(sys.argv[1]))), zip(range(int(sys.argv[2]))):
 #while x<= 5 and y <= 5:
@@ -36,7 +44,7 @@ for x in range(len(board)):
 	#global x,y
 	#if x<= 9:
 		#if board[x][y] == 1:
-			eb[x][y] = '*'
+			#eb[x][y] = '*'
 		#x = x + 1
 		#y = y + 1
 		#count()
@@ -44,9 +52,7 @@ for x in range(len(board)):
 	#	break
 	
 
-count()
-print("\n\n")
 
 for x in range(len(eb)):
-	print(*b2[x])
+	print(*eb[x])
 
