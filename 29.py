fdf406189f853a64521ee06008c99a83d019d097
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-09-29 12:38:56 -0400

    mines

---
+++ 29.py
@@ -0,0 +1,34 @@
+import sys
+import random as r
+board = []
+eb = []
+count = 0
+x = 0
+y = 0
+
+
+board = [[0]*int(sys.argv[1]) for x in range (int(sys.argv[2]))]
+eb = [[0]*int(sys.argv[1]) for x in range (int(sys.argv[2]))]
+print(board)
+
+while count < int(sys.argv[3]):
+	board[r.randrange(0,int(sys.argv[1]))][r.randrange(0,int(sys.argv[2]))] = 1
+	count = count+1
+
+print(board)
+
+
+#for x, y in zip(range(int(sys.argv[1]))), zip(range(int(sys.argv[2]))):
+#while x<= int(sys.argv[1]) and y <= int(sys.argv[2]):
+	if board[1][3] ==1:
+		eb[x][y] = '*'
+		x = x + 1
+		y = y + 1
+
+
+for x in range(len(board)):
+	print(*board[x])
+
+for x in range(len(eb)):
+	print(*eb[x])
+
