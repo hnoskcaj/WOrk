d563e8442aac39487dc463bbe351a48893cdf947
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-09-29 19:36:26 -0400

    1.2

--- 29.py
+++ 29.py
@@ -1,7 +1,7 @@
 import sys
 import random as r
 board = []
-eb = []
+b2 = []
 count = 0
 x = 0
 y = 0
@@ -19,24 +19,34 @@ print(board)
 
 
 #for x, y in zip(range(int(sys.argv[1]))), zip(range(int(sys.argv[2]))):
-#while x<= int(sys.argv[1]) and y <= int(sys.argv[2]):
-	#if board[1][3] ==1:
-	#	eb[x][y] = '*'
-	#	x = x + 1
-	#	y = y + 1
+while x<= int(sys.argv[1]) and y <= int(sys.argv[2]):
+	if board[1][3] ==1:
+		b2[x][y] = '*'
+	#x = x + 1
+	#y = y + 1
 
 for x in range(len(board)):
 	print(*board[x])
 
 #for x, y in zip(range(int(sys.argv[1]))), zip(range(int(sys.argv[2]))):
-while x<= 5 and y <= 5:
-	if board[x][y] ==1:
-		eb[x][y] = '*'
-	x = x + 1
-	y = y + 1
+#while x<= 5 and y <= 5:
 
+
+#def count():
+	#global x,y
+	#if x<= 9:
+		#if board[x][y] == 1:
+			eb[x][y] = '*'
+		#x = x + 1
+		#y = y + 1
+		#count()
+	#else:
+	#	break
+	
+
+count()
 print("\n\n")
 
 for x in range(len(eb)):
-	print(*eb[x])
+	print(*b2[x])
 
