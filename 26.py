3a529a4e8907b6c6ccb1e8587810d64fef13f06f
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-09-29 12:38:21 -0400

    condensed

--- 26.py
+++ 26.py
@@ -8,11 +8,7 @@ print(lits)
 while count < 100:
 	first.append(int(str(lits[count])[:1]))
 	count = count + 1
-#first.append(int(str(lits[1])[:1]))
-#first.append(int(str(lits[2])[:1]))
-#first.append(int(str(lits[3])[:1]))
-#first.append(int(str(lits[4])[:1]))
-#first.append(int(str(lits[5])[:1]))
+
 first.sort(key = int)
 
 one = 0
