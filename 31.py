ba35a4144f0f4c2e628a14b6588557603cf84439
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-10-02 11:26:52 -0400

    3.1

--- 31.py
+++ 31.py
@@ -13,34 +13,34 @@ while count < int(sys.argv[3]):
 	xrand = r.randrange(0,int(sys.argv[1])-1)
 	yrand = r.randrange(0,int(sys.argv[2])-1)
 	#print(xrand,yrand,len(board),len(board[0]))
-	board[xrand][yrand] = *
-	if board[xrand+1][yrand] != *:
+	board[xrand][yrand] = '*'
+	if board[xrand+1][yrand] != '*':
 		board[xrand+1][yrand] += 1
 	
-	if board[xrand-1][yrand] != *:
+	if board[xrand-1][yrand] != '*':
 		board[xrand-1][yrand] += 1
 	
-	if board[xrand][yrand+1] != *:
+	if board[xrand][yrand+1] != '*':
 		board[xrand][yrand+1] += 1
 	
-	if board[xrand][yrand-1] != *:
+	if board[xrand][yrand-1] != '*':
 		board[xrand][yrand-1] += 1
 	
-	if board[xrand+1][yrand+1] != *:
+	if board[xrand+1][yrand+1] != '*':
 		board[xrand+1][yrand+1] += 1
 	
-	if board[xrand+1][yrand-1] != *:
+	if board[xrand+1][yrand-1] != '*':
 		board[xrand+1][yrand-1] += 1
 	
-	if board[xrand-1][yrand+1] != *:
+	if board[xrand-1][yrand+1] != '*':
 		board[xrand-1][yrand+1] += 1
 	
-	if board[xrand-1][yrand-1] != *:
+	if board[xrand-1][yrand-1] != '*':
 		board[xrand-1][yrand-1] += 1
 	
 	count = count+1
 
-print(board)
+print(board,"\n")
 
 for x in range(len(board)):
 	print(*board[x])
