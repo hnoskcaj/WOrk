e5cab876c12d0dd5e8c27d24cca65583f09e09be
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-10-02 15:04:41 -0400

    3.3

--- 31.py
+++ 31.py
@@ -3,21 +3,22 @@ import random as r
 board = []
 count = 0
 bomb = []
+a = 0
 
 
 
 board = [[0]*int(sys.argv[1]) for x in range (int(sys.argv[2]))]
-eb = [[0]*int(sys.argv[1]) for x in range (int(sys.argv[2]))]
 print(board)
 
 while count < int(sys.argv[3]):
 	xrand = r.randrange(0,int(sys.argv[1])-1)
 	yrand = r.randrange(0,int(sys.argv[2])-1)
-	if xrand and yrand not in bomb:
+	#a = xrand + yrand
+	if [(xrand,yrand)] not in bomb:
 	#print(xrand,yrand,len(board),len(board[0]))
 		board[xrand][yrand] = '*'
-		bomb.append(xrand)
-		bomb.append(yrand)
+		bomb.append((xrand,yrand))
+		#bomb.append(yrand)
 		if board[xrand+1][yrand] != '*':
 			board[xrand+1][yrand] += 1
 		
