259006ce5029c0befded06fc9d153b051d06ce8c
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-10-02 14:38:30 -0400

    3.2

--- 31.py
+++ 31.py
@@ -2,6 +2,7 @@ import sys
 import random as r
 board = []
 count = 0
+bomb = []
 
 
 
@@ -12,33 +13,36 @@ print(board)
 while count < int(sys.argv[3]):
 	xrand = r.randrange(0,int(sys.argv[1])-1)
 	yrand = r.randrange(0,int(sys.argv[2])-1)
+	if xrand and yrand not in bomb:
 	#print(xrand,yrand,len(board),len(board[0]))
-	board[xrand][yrand] = '*'
-	if board[xrand+1][yrand] != '*':
-		board[xrand+1][yrand] += 1
+		board[xrand][yrand] = '*'
+		bomb.append(xrand)
+		bomb.append(yrand)
+		if board[xrand+1][yrand] != '*':
+			board[xrand+1][yrand] += 1
+		
+		if board[xrand-1][yrand] != '*':
+			board[xrand-1][yrand] += 1
 	
-	if board[xrand-1][yrand] != '*':
-		board[xrand-1][yrand] += 1
+		if board[xrand][yrand+1] != '*':
+			board[xrand][yrand+1] += 1
 	
-	if board[xrand][yrand+1] != '*':
-		board[xrand][yrand+1] += 1
+		if board[xrand][yrand-1] != '*':
+			board[xrand][yrand-1] += 1
 	
-	if board[xrand][yrand-1] != '*':
-		board[xrand][yrand-1] += 1
+		if board[xrand+1][yrand+1] != '*':
+			board[xrand+1][yrand+1] += 1
 	
-	if board[xrand+1][yrand+1] != '*':
-		board[xrand+1][yrand+1] += 1
+		if board[xrand+1][yrand-1] != '*':
+			board[xrand+1][yrand-1] += 1
 	
-	if board[xrand+1][yrand-1] != '*':
-		board[xrand+1][yrand-1] += 1
+		if board[xrand-1][yrand+1] != '*':
+			board[xrand-1][yrand+1] += 1
 	
-	if board[xrand-1][yrand+1] != '*':
-		board[xrand-1][yrand+1] += 1
+		if board[xrand-1][yrand-1] != '*':
+			board[xrand-1][yrand-1] += 1
 	
-	if board[xrand-1][yrand-1] != '*':
-		board[xrand-1][yrand-1] += 1
-	
-	count = count+1
+		count = count+1
 
 print(board,"\n")
 
