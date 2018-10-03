614f496c64885a08a163a3e4a5d42d3c693a6ca0
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-10-03 10:51:26 -0400

    3.4

--- 31.py
+++ 31.py
@@ -5,40 +5,47 @@ count = 0
 bomb = []
 a = 0
 
+width = int(sys.argv[1])+2 
+height = int(sys.argv[2])+2
 
-
-board = [[0]*int(sys.argv[1]) for x in range (int(sys.argv[2]))]
+board = [[0]*width for x in range (height)]
 print(board)
 
 while count < int(sys.argv[3]):
-	xrand = r.randrange(0,int(sys.argv[1])-1)
-	yrand = r.randrange(0,int(sys.argv[2])-1)
+	xrand = r.randrange(1,height-1)
+	yrand = r.randrange(1,width-1)
+
 	#a = xrand + yrand
-	if [(xrand,yrand)] not in bomb:
+	if (xrand,yrand) not in bomb:
 	#print(xrand,yrand,len(board),len(board[0]))
 		board[xrand][yrand] = '*'
 		bomb.append((xrand,yrand))
 		#bomb.append(yrand)
-		if board[xrand+1][yrand] != '*':
-			board[xrand+1][yrand] += 1
-		
-		if board[xrand-1][yrand] != '*':
-			board[xrand-1][yrand] += 1
-	
+		print(xrand,yrand,"---------")
+
 		if board[xrand][yrand+1] != '*':
 			board[xrand][yrand+1] += 1
 	
 		if board[xrand][yrand-1] != '*':
 			board[xrand][yrand-1] += 1
+
+
+		if board[xrand+1][yrand] != '*':
+			board[xrand+1][yrand] += 1
 	
 		if board[xrand+1][yrand+1] != '*':
 			board[xrand+1][yrand+1] += 1
 	
 		if board[xrand+1][yrand-1] != '*':
 			board[xrand+1][yrand-1] += 1
+
+
 	
 		if board[xrand-1][yrand+1] != '*':
 			board[xrand-1][yrand+1] += 1
+
+		if board[xrand-1][yrand] != '*':
+			board[xrand-1][yrand] += 1
 	
 		if board[xrand-1][yrand-1] != '*':
 			board[xrand-1][yrand-1] += 1
@@ -47,5 +54,5 @@ while count < int(sys.argv[3]):
 
 print(board,"\n")
 
-for x in range(len(board)):
+for x in range(1,len(board)-1):
 	print(*board[x])
