c28c21095066f7d702c3e93600acd00d5fdcec7f
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-10-19 09:43:39 -0400

    mandelbrpt

---
+++ 43.py
@@ -0,0 +1,23 @@
+
+import sys
+import math
+import random as r
+board = []
+count = 0
+bomb = []
+gameboard = []
+a = 0
+clear = []
+zeros = 0
+
+width = int(sys.argv[1])+2 
+height = int(sys.argv[2])+2
+
+board = [[0]*width for x in range (height)]
+print(board)
+
+c 
+if sqrt(z[1]**2 + z[2]**2) >= 2:
+	board[][] = count
+z[n+1] = z[n]**2 + C
+
