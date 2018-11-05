46a2dc2e0068f9cb69fb4265049d2c6505b1f403
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-11-05 12:07:05 -0500

    0.1.1

--- 49.py
+++ 49.py
@@ -1,2 +1,14 @@
+import csv
+with open('elements.csv') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=',')
+	line_count = 0
+	for x in csv_reader:
+			{x[2]} = El({x[0]},{x[1]} ,{x[3]},)
+			line_count += 1
+	
+
 class El:
-	def__init__(self,name,abr,weight)
+	def __init__(self,name,num,weight):
+		self.name = name
+		self.num = num
+		self.weight = weight
