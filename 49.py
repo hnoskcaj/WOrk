7093737ff2355d0fdf996c3187aa0de4ec9223fd
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-11-07 10:41:09 -0500

    0.1.2

--- 49.py
+++ 49.py
@@ -12,3 +12,14 @@ class El:
 		self.name = name
 		self.num = num
 		self.weight = weight
+
+	def stats(self):
+		return "Name:"+ self.name+"\nAtomic Number:"+str(self.num)+"\nAtomic weight:"+str(self.wieght)
+
+
+x= input("Type a chemical formula to find the wieght: ").split()
+
+for a in len(x):
+	if sys.argv[a+1] = int:
+		sys.argv[a].stats()
+
