8f4f7e6471ce64075f21b9d8f0c74b424792510f
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-11-07 11:00:57 -0500

    0.1.3

--- 49.py
+++ 49.py
@@ -1,25 +1,28 @@
-import csv
-with open('elements.csv') as csv_file:
-	csv_reader = csv.reader(csv_file, delimiter=',')
-	line_count = 0
-	for x in csv_reader:
-			{x[2]} = El({x[0]},{x[1]} ,{x[3]},)
-			line_count += 1
-	
-
 class El:
 	def __init__(self,name,num,weight):
 		self.name = name
 		self.num = num
 		self.weight = weight
 
-	def stats(self):
-		return "Name:"+ self.name+"\nAtomic Number:"+str(self.num)+"\nAtomic weight:"+str(self.wieght)
+	def weights(self):
+		return self.wieght
+
+
+import csv
+with open('elements.csv') as csv_file:
+	csv_reader = csv.reader(csv_file, delimiter=',')
+	line_count = 0
+	for x in csv_reader:
+			x[2] = El(x[0],x[1],x[3])
+			line_count += 1
+
 
 
 x= input("Type a chemical formula to find the wieght: ").split()
-
-for a in len(x):
-	if sys.argv[a+1] = int:
-		sys.argv[a].stats()
+sumList = []
+for a in range(0,len(x)):
+	if sys.argv[a+1] == int:
+		sumList.append(sys.argv[a].weights())
+	elif sys.argv[a+2] == int:
+		sumList.append(sys.argv[(a)+(a+1)].weights())
 
