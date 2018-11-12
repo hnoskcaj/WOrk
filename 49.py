9a8329677ed824f738a155b3a1e0a12b5a4a3d91
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-11-11 19:05:49 -0500

    2.0?

--- 49.py
+++ 49.py
@@ -1,7 +1,25 @@
-import csv
+#Jackson Haile 11/12/18
 
+#this code reads an imported csv file, breaks the different inputs into classes, and lets you call them to find information
+#about a chamical formula. Type the name of the file you want to import (it should be called "elements.csv"), and run the program.
+
+#Sources: Stacks overflow to find .isnumeric/.isupper functions
+
+#On my honor i have niether given nor recieved unauthorized aid
+#-Jackson
+
+
+
+
+
+#imports file-reader
+import csv
+#takes file name to input
+file = input("Type the file name you want to import here:")
+#sets class of elements
 class El:
 	def __init__(self,name,sym,num,weight,boil,melt):
+		#puts variables from the table setup into the elements based on their row and name
 		self.name = name
 		self.sym = sym
 		self.num = num
@@ -10,49 +28,70 @@ class El:
 		self.melt = melt
 
 
-
+#create class table
 class table:
 	def __init__(self):
 		self.elems = []
-		with open('elements.csv') as csv_file:
+		with open(file) as csv_file:
 			csv_reader = csv.reader(csv_file, delimiter=',')
 			line_count = 0
+			#goes throgh each spot in the csv and sets the spots to variables
 			for z in csv_reader:
 				name = z[0] 
 				sym = z[2] 
 				num = z[1] 
 				weight = z[3]
 				boil = z[4]
-				melt = z[4]
+				#tests to see if there is a value at z[5]
+				try:
+					melt = z[5]
+				except IndexError:
+					melt = 0
 				self.elems.append(El(name,sym,num,weight,boil,melt))
 
-
-x= input("Type a chemical formula to find the wieght: ").split()
+#takes user input and breaks it at the spaces to make a list, x
+x= input("\n\nType a chemical formula to find the wieght\n(Put a space between each element and number) \nie: H 2 O\n: ").split()
+#sets lists and variables
 meltList = 0
 boilList = 0
 sumList = []
 t = table()
 result = None
+#goes throught the spots in the input
 for a in range(0,len(x)):
+	#checks that there is somehting to compare
 	if a+1<len(x):
+		#check to see is a is the whole element(and the next starts and is therefore upercase)
 		if x[a+1].isupper():
+			#references class elements(El)
 			for i in t.elems:
+				#if the input matches the symbol
 				if x[a] == i.sym:
+					#add the weight once becasue the input was only one  part long w/ no number after it
 					sumList.append(float(i.weight))
+					#adds boiling point
 					if boilList < float(i.boil):
 						boilList = float(i.boil)
+						#adds melting point
 					if meltList < float(i.melt):
 						meltList = float(i.melt)
-
+			#if the next spot is lower that needs to be included in the symbol
 		elif x[a+1].islower():
-			if x[a+2].isnumeric():
-				for i in t.elems:
-					if (x[a]+x[a+1]) == i.sym:
-						sumList.append(float(i.weight)*int(x[a+2]))
-						if boilList < float(i.boil):
-							boilList = float(i.boil)
-						if meltList < float(i.melt):
-							meltList = float(i.melt)
+			#checks to see if there is a number next that references this subsatnce
+			if a+2<len(x):
+				if x[a+2].isnumeric():
+					for i in t.elems:
+						#adds the first and second letters of the symbol
+						if (x[a]+x[a+1]) == i.sym:
+							#multiplies weight my the number of that element
+							sumList.append(float(i.weight)*int(x[a+2]))
+							#sets boiling point if it is the highest
+							if boilList < float(i.boil):
+								boilList = float(i.boil)
+								#sets as melting point if it is the highest
+							if meltList < float(i.melt):
+								meltList = float(i.melt)
+				#if there is inly one two letter long element
 			else:
 				for i in t.elems:
 					if x[a] == i.sym:
@@ -61,6 +100,7 @@ for a in range(0,len(x)):
 							boilList = float(i.boil)
 						if meltList < float(i.melt):
 							meltList = float(i.melt)
+		#is the smbol is one letter long and there is a number following it
 
 		elif x[a+1].isnumeric():
 			for i in t.elems:
@@ -70,7 +110,7 @@ for a in range(0,len(x)):
 						boilList = float(i.boil)
 					if meltList < float(i.melt):
 						meltList = float(i.melt)
-
+ #if the input is only one letter long, then it just adds that with the weight and boiling/melting point
 	else:
 		for i in t.elems:
 				if x[a] == i.sym:
@@ -79,39 +119,7 @@ for a in range(0,len(x)):
 						boilList = float(i.boil)
 					if meltList < float(i.melt):
 						meltList = float(i.melt)
+#print out values of total weight, highest boiling point, and highest melting point
+#(i was going to try and calculate the actual boiling and melting points, but that ended up being too hard)
 
-print(sum(sumList),"\n"+str(boilList))
-
-
-# import csv
-# with open('elements.csv') as csv_file:
-# 	csv_reader = csv.reader(csv_file, delimiter=',')
-# 	line_count = 0
-# 	for x in csv_reader:
-# 			print(x[2])
-# 			x[2] = El(x[0],x[1],x[3])
-# 			line_count += 1
-
-# summ = 0
-
-# x= input("Type a chemical formula to find the wieght: ").split()
-# sumList = []
-# print(x[0])
-# element = El("H",5,1)
-# for a in range(0,len(x)):
-# 	if a+1<len(x):
-# 		if x[a+1] == int:
-# 			sumList.append(x[a].weight)
-# 	elif a+2<len(x):
-# 		if x[a+2] == int:
-# 			sumList.append(x[(a)+(a+1)].weight)
-# 	else:
-# 		sumList.append(x[0].weight)
-
-# print(sumList)
-# for a in range(0,len(sumList)):
-# 	summ = summ + sumList[a]
-
-# print(summ)
-
-
+print("The attomic weight of this molecule is:",sum(sumList),"\nThe Highest boiling point in this substance is",str(boilList),"\nThe Highest melting point in thes substance is:",str(meltList))
