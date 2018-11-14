0346dead34fd58c885c7a31364689b2d0a956aea
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-11-14 10:52:07 -0500

    0

--- 50.py
+++ 50.py
@@ -1,11 +1,21 @@
-import csv
+from PIL import Image
+import sys
 
-with open('elements.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
-    line_count = 0
-    for row in csv_reader:
-       
-            print(f'Column names are {", ".join(row)}')
-          
-       
-    print(f'Processed {line_count} lines.')
+
+im = Image.open(sys.argv[1])
+x,y = im.size
+int(x)
+int(y)
+print(x,y)
+pix = im.load()
+for i in range(x):
+	for j in range(y):
+		r,g,b = (pix[i,j])
+		# print("loops")
+		# print(i,j)
+		pix[i,j] = (255-r,255-g,255-b)
+		# r = 250
+		# g = 250
+		# b = 250
+		im.putpixel((i,j),pix[i,j])
+im.show()
