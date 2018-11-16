4eb2decaf9d9c801bdad46de9507e05a127f1923
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-11-15 19:58:23 -0500

    yuh

--- 50.py
+++ 50.py
@@ -1,5 +1,50 @@
 from PIL import Image
 import sys
+import math
+
+
+im = Image.open(sys.argv[1]) 
+x,y = im.size
+int(x)
+int(y)
+pix = im.load()
+for i in range(0,x):
+	for j in range(0,y):
+		r,g,b = (pix[i,j])
+	
+		r = r + math.sin(i/x) *125
+		g = g + math.sin((i/x)+1) *125
+		b = b + math.sin((i/x)+2) *125
+		
+		# print(10*(math.asin(i/x)))
+		im.putpixel((i,j),(int(r),int(b),int(g)))
+im.show()
+
+#------------------------------------------------------------------
+
+im = Image.open(sys.argv[1]) 
+x,y = im.size
+int(x)
+int(y)
+pix = im.load()
+for i in range(0,x):
+	for j in range(0,y):
+		r,g,b = (pix[i,j])
+	
+		r = r + math.sin(i/10) *125+125
+		g = g + math.sin((i/10)+1) *125+125
+		b = b + math.sin((i/10)+2) *125+125
+
+		# print(10*(math.asin(i/x)))
+		im.putpixel((i,j),(int(r),int(b),int(g)))
+im.show()
+
+#------------------------------------------------------------------
+
+
+
+from PIL import Image
+import sys
 
 
 im = Image.open(sys.argv[1])
@@ -19,3 +64,4 @@ for i in range(x):
 		# b = 250
 		im.putpixel((i,j),pix[i,j])
 im.show()
+
