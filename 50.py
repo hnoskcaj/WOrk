209e5a117de759a12b2f4bab3d64cec8a4b75673
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2018-11-16 10:46:33 -0500

    final

--- 50.py
+++ 50.py
@@ -1,67 +1,73 @@
+#import pillow and commandline arguments
 from PIL import Image
 import sys
 import math
+#big rainbow effect
 
-
+#open image form command line
 im = Image.open(sys.argv[1]) 
+#read image size and set to cariables
 x,y = im.size
 int(x)
 int(y)
 pix = im.load()
+#go through all of the pixles, read the rgb values, and offset them
 for i in range(0,x):
 	for j in range(0,y):
 		r,g,b = (pix[i,j])
-	
+	#set the colors to the color plus a rainbow the length of the picture
 		r = r + math.sin(i/x) *125
 		g = g + math.sin((i/x)+1) *125
 		b = b + math.sin((i/x)+2) *125
 		
-		# print(10*(math.asin(i/x)))
+		# print image
 		im.putpixel((i,j),(int(r),int(b),int(g)))
 im.show()
 
 #------------------------------------------------------------------
 
-im = Image.open(sys.argv[1]) 
+#rap god effect
+
+#open image form command line
+im = Image.open(sys.argv[2]) 
+#read image size and set to cariables
 x,y = im.size
 int(x)
 int(y)
 pix = im.load()
+#go through all of the pixles, read the rgb values, and offset them
 for i in range(0,x):
 	for j in range(0,y):
 		r,g,b = (pix[i,j])
-	
-		r = r + math.sin(i/10) *125+125
-		g = g + math.sin((i/10)+1) *125+125
-		b = b + math.sin((i/10)+2) *125+125
+	#set the colors to look like lines that are small rainbows across the screen (eminem rap god effect)
+		r = r + math.sin(i) *125+125
+		g = g + math.sin((i)+1) *125+125
+		b = b + math.sin((i)+2) *125+125
 
-		# print(10*(math.asin(i/x)))
+		##print image
 		im.putpixel((i,j),(int(r),int(b),int(g)))
 im.show()
 
 #------------------------------------------------------------------
 
 
+#inverse effect
 
-from PIL import Image
-import sys
-
-
-im = Image.open(sys.argv[1])
+#open image form command line
+im = Image.open(sys.argv[3])
+#read image size and set to cariables
 x,y = im.size
 int(x)
 int(y)
 print(x,y)
 pix = im.load()
+#go through all of the pixles, read the rgb values, and offset them
 for i in range(x):
 	for j in range(y):
 		r,g,b = (pix[i,j])
-		# print("loops")
-		# print(i,j)
+		#set the colors to the inverse of what they originaly were
 		pix[i,j] = (255-r,255-g,255-b)
-		# r = 250
-		# g = 250
-		# b = 250
+		# print image
 		im.putpixel((i,j),pix[i,j])
 im.show()
 
