8302d8ab8ac0da004d4fba1ed9af97f8e5048974
hnoskcaj <43040830+hnoskcaj@users.noreply.github.com>
2019-01-23 12:27:38 -0500

    1.1

--- 68.py
+++ 68.py
@@ -6,27 +6,30 @@ from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 
 
-class Book(App):
-	def build(self):
-		self.button = Button(text = "hi", background_color = (3,3,1,1), font_size = 100)
-		# self.button.bind(on_release=start)
-		return Button
+############################################################################
 
-# def start():
-# 	print("go!")
-
-if __name__ == "__main__":
-	Book().run()
-
+#classic button w/ color change
 
 # class Book(App):
 # 	def build(self):
-# 		f = FloatLayout()
-# 		s = Scatter()
-# 		l = Label(text = "Hello world", font_size = 150)
+# 		return Button(text = "Start", background_color=(3,3,1,1), font_size=100)
+		
 
-# 		f.add_widget(s)
-# 		s.add_widget(l)
-# 		return f
+# if __name__ == "__main__":
+# 	Book().run()
 
-# Book().run()
+############################################################################
+
+#moveable floate w/ turning (option+rclick)
+
+class Book(App):
+	def build(self):
+		f = FloatLayout()
+		s = Scatter()
+		l = Label(text = "Hello world", font_size = 150)
+
+		f.add_widget(s)
+		s.add_widget(l)
+		return f
+
+Book().run()
